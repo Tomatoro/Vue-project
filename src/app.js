67fb753a68801df5aa57386c3f1cdd688b39dc50
Tomatoro <15313366520@163.com>
2017-12-09 21:54:11 +0800

    懒加载+载入提示

--- src/app.js
+++ src/app.js
@@ -16,7 +16,28 @@ import app from './app.vue'
 let vm = new Vue({
   el:'#app',
   router,
-  render: c => c(app)
+  render: c => c(app),
+  created(){
+    // 添加请求拦截器
+    this.axios.interceptors.request.use(function (config) {
+      // 在发送请求之前做些什么
+      this.$indicator.open('加载中...');
+      return config;
+    }.bind(this), function (error) {
+      // 对请求错误做些什么
+      return Promise.reject(error);
+    });
+
+    // 添加响应拦截器
+    this.axios.interceptors.response.use(function (response) {
+      // 对响应数据做点什么
+      this.$indicator.close();
+      return response;
+    }.bind(this), function (error) {
+      // 对响应错误做点什么
+      return Promise.reject(error);
+    });
+  }
 })
  // createElement帮我们处理组件，并把处理的结果返回，渲染到#app中。不是dom中的createElement
 // render: function (createElement) {
